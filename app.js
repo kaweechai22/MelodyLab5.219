0b62a137a9d2c6f63ab6bbb155d7acc6d2829fc6
kaweechai22 <cacarosjampa@gmail.com>
2026-06-18 17:20:02 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const $=id=>document.getElementById(id);
-/* v5.223 standing wave physics design */
+/* v5.224 standing wave tube pressure */
 const rebuiltTopicModes = new Set(["soundReflection","soundRefraction","soundDiffraction","soundInterference","superposition","beatsViz","standingAir","resonanceViz","resonanceAirHarmonics","dopplerViz","shockWave","soundIntensity","soundIntensityLevel","soundLevelHearing","harmonicsViz","noisePollution","applicationsSound"]);
 const soundTopicModes = rebuiltTopicModes;
 
